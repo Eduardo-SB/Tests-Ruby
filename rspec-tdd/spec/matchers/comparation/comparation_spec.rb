8b1eef9b9c977cdc29f05cb03eb1119830019ff4
Eduardo-SB <eduardosb95@gmail.com>
2020-05-06 15:22:37 -0300

    Matchers - comparation

---
+++ rspec-tdd/spec/matchers/comparation/comparation_spec.rb
@@ -0,0 +1,31 @@
+describe 'Matchers - Comparation' do
+  it '#equal = Test is the same object' do
+    x = 'ruby'
+    y = 'ruby'
+
+    expect(x).not_to equal(y)  
+    expect(x).to equal(x)  
+  end
+
+  it '#be = Test is the same object' do
+    x = 'ruby'
+    y = 'ruby'
+
+    expect(x).not_to be(y)  
+    expect(x).to be(x)  
+  end
+
+  it '#eql = Test value' do
+    x = 'ruby'
+    y = 'ruby'
+
+    expect(x).to eql(y)
+  end
+
+  it '#eq = Test value' do
+    x = 'ruby'
+    y = 'ruby'
+
+    expect(x).to eq(y)
+  end
+end
